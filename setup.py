0f458d2b8b1e0137d50b4a916045b30a94d8fbac
Chris Bridge <chrisbridge44@googlemail.com>
2024-02-18 00:48:53 -0500

    Add option for single bit compression

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
         'libjpeg': [
             'pylibjpeg>=1.4',
             'pylibjpeg-libjpeg>=1.3',
-            'pylibjpeg-openjpeg>=1.2'
+            'pylibjpeg-openjpeg>=2.1.0'
         ],
     },
 )

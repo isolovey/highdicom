8b467aa373005d979967ed8ba49dd0c10047274f
Igor Solovey <igor.solovey@uwo.ca>
2024-09-12 12:45:13 -0400

    Handle missing and empty elements in frame-level functional groups
    - Preserve frame index when appending unassigned data elements
    - Handle case when only one frame has an attribute set

--- src/highdicom/legacy/sop.py
+++ src/highdicom/legacy/sop.py
@@ -3,10 +3,12 @@
 import logging
 from collections import defaultdict
 from datetime import datetime
-from typing import Any, Dict, List, Optional, Sequence
+from typing import Any, Dict, List, Optional, Sequence, Tuple
 
 import pydicom
+from pydicom import DataElement
 from pydicom.datadict import tag_for_keyword
+from pydicom.tag import Tag
 from pydicom.dataset import Dataset
 from pydicom.encaps import encapsulate
 from pydicom.uid import (
@@ -165,11 +167,11 @@ def _convert_legacy_to_enhanced(
     pixel_representation = sf_datasets[0].PixelRepresentation
     volumetric_properties = 'VOLUME'
     unique_image_types = set()
-    unassigned_dataelements: Dict[str, List[Dataset]] = defaultdict(list)
+    unassigned_dataelements: Dict[Tag, List[Tuple[int, DataElement]]] = defaultdict(list)
 
     # Per-Frame Functional Groups
     perframe_items = []
-    for ds in sf_datasets:
+    for frame_index, ds in enumerate(sf_datasets):
         perframe_item = Dataset()
 
         # Frame Content (M)
@@ -350,7 +352,8 @@ def _convert_legacy_to_enhanced(
                 mf_dataset.add(da)
             else:
                 if tag not in ignored_attributes:
-                    unassigned_dataelements[tag].append(da)
+                    unassigned_dataelements[tag].append((frame_index, da))
+
 
     # All remaining unassigned attributes will be collected in either the
     # UnassignedSharedConvertedAttributesSequence or the
@@ -362,12 +365,12 @@ def _convert_legacy_to_enhanced(
         for _ in range(len(sf_datasets))
     ]
     for _tag, dataelements in unassigned_dataelements.items():
-        values = [str(da.value) for da in dataelements]
+        values = [str(da.value) for _, da in dataelements]
         unique_values = set(values)
-        if len(unique_values) == 1:
-            unassigned_shared_ca_item.add(dataelements[0])
+        if len(dataelements) == len(sf_datasets) and len(unique_values) == 1:
+            unassigned_shared_ca_item.add(dataelements[0][1])
         else:
-            for i, da in enumerate(dataelements):
+            for i, da in dataelements:
                 unassigned_perframe_ca_items[i].add(da)
 
     mf_dataset.ImageType = list(list(unique_image_types)[0])
